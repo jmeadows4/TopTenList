eb932613dad5f244bc59314837c50c1e07765119
Joshua Meadows <jmeadows4@mail.csuchico.edu>
2020-04-01 17:59:14 -0700

    Add functions to display forward/backward

--- TopTenList.cpp
+++ TopTenList.cpp
@@ -4,10 +4,23 @@
 #include "Hyperlink.h"
 
 using std::string;
+using std::cout;
+using std::endl;
 
 TopTenList::TopTenList()
 {
     _list.resize(10);
+}
+
+void TopTenList::display_forward(){
+    for(int i = 0; i < _list.size(); i++){
+      cout<< i + 1 << ". "<< _list[i].text << endl;
+    }
+}
+void TopTenList::display_backward(){
+    for(int i = _list.size()-1; i >= 0; i--){
+      cout<< i + 1 << ". "<< _list[i].text << endl;
+    }
 }
 
 void TopTenList::set_at(int index, Hyperlink link)
--- TopTenList.h
+++ TopTenList.h
@@ -3,17 +3,24 @@
 #define TOPTENLIST_H
 #include <string>
 #include <vector>
+#include<iostream>
 #include "Hyperlink.h"
+#include "IDirectionable.h"
 
 using std::string;
 using std::vector;
+using std::cout;
+using std::endl;
 
-class TopTenList
+class TopTenList  : public IDirectionable
 {
   private:
     vector<Hyperlink> _list;
 
   public:
+    TopTenList();
+    void display_forward();
+    void display_backward();
     void set_at(int index, Hyperlink link);
     Hyperlink get(int index);
 };
